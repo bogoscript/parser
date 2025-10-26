83f4efddd2a2f1a5201a4b895a649edec80fe2b4
faretek <ldashunderscoreo@gmail.com>
2025-10-26 10:41:00 +0000

    feat: print_str, strings, number
    copied from gigascript

--- grammar.js
+++ grammar.js
@@ -9,9 +9,47 @@
 
 module.exports = grammar({
   name: "bogoscript",
-
+  extras: $ => [
+    /\s/,
+    $.comment_line,
+    $.comment_block,
+  ],
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(
+      $.statement
+    ),
+    statement: $ => choice(
+      $.statement_print_str
+    ),
+    statement_print_str: $ => seq("print_str", $.expression, ";"),
+    expression: $ => choice($.literal),
+
+    literal: $ => choice($.literal_number, $.literal_string),
+    literal_number: $ => /\d+/,
+    literal_string: $ => seq(
+      '"', repeat($.string_char), token.immediate('"')
+    ),
+    string_char: $ => choice(
+      /[^\\\n]/, // anything that isn't a backslash or newline
+      seq("\\", choice(
+        "\\",
+        "n",
+        "\"",
+        "'",
+        "b",
+        "f",
+        "r",
+        "t",
+        seq("u", $.hexdigit, $.hexdigit, $.hexdigit, $.hexdigit)))
+    ),
+
+    comment_line: $ => seq("//", /[^\n]*/),
+    comment_block: $ => seq("/*",
+      token.immediate(repeat(choice(/[^*]/, /\*[^/]/))), // thanks mr ai...
+      token.immediate("*/")),
+
+    letter: $ => token.immediate(/[A-z]/),
+    hexdigit: $ => token.immediate(/[0-9a-fA-F]/),
+    digit: $ => token.immediate(/[0-9]/),
   }
 });
